5f2ac151595078fd05feb045c1f31d98292d8158
José Luis Jiménez <78834111+joseljim@users.noreply.github.com>
2022-02-27 17:33:37 -0600

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,4 +1,4 @@
-//---------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
 // MIT License
 //
 // Copyright (c) 2022 José Luis Jiménez Arévalo, Eduardo García Olmos
@@ -20,7 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-//---------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
 
 #include <stdio.h>
 #include "board.h"
